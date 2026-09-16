0727774777e580507bc9ff25e11eae8f5272377e
Mohd Sohrab Alam <120323043+mohdsohrab2003@users.noreply.github.com>
2026-09-16 21:30:48 +0530

    Add production URL to CORS configuration

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -6,7 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // CORS
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173'],
+    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://stunning-tranquility-production-0719.up.railway.app/'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
