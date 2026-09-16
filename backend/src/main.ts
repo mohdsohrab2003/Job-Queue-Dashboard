9353224361a3a127b6b90185fa081bfbe6aa8f8a
Mohd Sohrab Alam <120323043+mohdsohrab2003@users.noreply.github.com>
2026-09-16 21:36:46 +0530

    Update CORS origins and import ValidationPipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,16 +1,20 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  // CORS
+
   app.enableCors({
-    origin: ['http://localhost:5173', 'http://127.0.0.1:5173', 'https://stunning-tranquility-production-0719.up.railway.app/'],
+    origin: [
+      'http://localhost:5173',
+      'http://127.0.0.1:5173',
+      'https://job-queue-dashboard-production-4c80.up.railway.app',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
-  // VALIDATION GLOBEL
+
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
@@ -18,6 +22,8 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 await bootstrap();
